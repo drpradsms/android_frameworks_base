e90aa744bd42d589e84b6117a260186ff087c8d6
Alex Cruz <du.alexcruz@gmail.com>
2018-11-27 03:46:23 -0600

    Move vendor blacklist overlays to ThemeAccentUtils
    Change-Id: I1b08a38e1cd2c734e68a3fbd9bde7ae7482368d5
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -282,6 +282,7 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.os.IParcelFileDescriptorFactory;
 import com.android.internal.os.SomeArgs;
 import com.android.internal.os.Zygote;
+import com.android.internal.statusbar.ThemeAccentUtils;
 import com.android.internal.telephony.CarrierAppUtils;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.ConcurrentUtils;
@@ -8510,13 +8511,6 @@ public class PackageManagerService extends IPackageManager.Stub
         }
     }
 
-    private String[] systemOverlayPackages = {"SysuiDarkTheme",
-                                              "DisplayCutoutEmulationCorner",
-                                              "DisplayCutoutEmulationDouble",
-                                              "DisplayCutoutEmulationNarrow",
-                                              "DisplayCutoutEmulationTall",
-                                              "DisplayCutoutEmulationWide"};
-
     private void scanDirLI(File scanDir, int parseFlags, int scanFlags, long currentTime) {
         final File[] files = scanDir.listFiles();
         if (ArrayUtils.isEmpty(files)) {
@@ -8542,7 +8536,7 @@ public class PackageManagerService extends IPackageManager.Stub
                 }
                 // Ignore vendor overlays that should live on system/overlay
                 if ((scanDir.getPath() == PACKAGE_OVERLAY_DIRS[1] || scanDir.getPath() == PRODUCT_OVERLAY_DIR)
-                        && Arrays.asList(systemOverlayPackages).contains(file.getName())){
+                        && Arrays.asList(ThemeAccentUtils.BLACKLIST_VENDOR_OVERLAYS).contains(file.getName())){
                     continue;
                 }
                 parallelPackageParser.submit(file, parseFlags);

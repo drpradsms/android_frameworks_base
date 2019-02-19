455d8368b9aef19ba708400b6937c9fa57ec164a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-19 10:10:54 -0300

    SystemUI: Refresh themes on dirty flash
    Change-Id: I54263e6196387ce9f5eb914e76b8d999bed6b7a2
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -90,6 +90,7 @@ import android.media.session.MediaController;
 import android.metrics.LogMaker;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Process;
@@ -4079,6 +4080,16 @@ public class StatusBar extends SystemUI implements DemoMode,
         Trace.endSection();
     }
 
+    private boolean themeNeedsRefresh(){
+        if (mContext.getSharedPreferences("systemui_theming", 0).getString(
+                "build_fingerprint", "").equals(Build.AOSIP_FINGERPRINT)){
+            return false;
+        }
+        mContext.getSharedPreferences("systemui_theming", 0).edit().putString(
+                "build_fingerprint", Build.AOSIP_FINGERPRINT).commit();
+        return true;
+    }
+
     /**
      * Switches theme from light to dark and vice-versa.
      */
@@ -4097,7 +4108,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         final boolean useDarkTheme = nightModeWantsDarkTheme;
         boolean useBlackAFTheme = useDarkTheme && (Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.USE_BLACKAF_THEME, 0, UserHandle.USER_CURRENT) == 1);
-        if (isUsingDarkTheme() != useDarkTheme) {
+        if (themeNeedsRefresh() || isUsingDarkTheme() != useDarkTheme) {
             mUiOffloadThread.submit(() -> {
                 unfuckBlackWhiteAccent();
                 ThemeAccentUtils.setLightDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useDarkTheme);

ffaa8ba87be5cc88dea2c295080eb6b7e37c26e9
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-04 11:28:25 -0300

    fwb: Add automatic theme based on time
    Change-Id: I5f8245fbd32156eb681ed0942abed6f0e8be3f68
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8112,6 +8112,12 @@ public final class Settings {
         public static final int THEME_MODE_DARK = 2;
 
         /**
+         * THEME_MODE value for time mode.
+         * @hide
+         */
+        public static final int THEME_MODE_TIME = 3;
+
+        /**
          * Whether screensavers are enabled.
          * @hide
          */

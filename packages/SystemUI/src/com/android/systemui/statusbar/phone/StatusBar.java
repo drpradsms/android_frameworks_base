1a900b29db44c992c99a1dbf2250970a105efc6c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-15 20:35:57 -0300

    Improve theming
    * Fix notification text
    * Move automatic theme based on time to LiveDisplay class
    * Improve overlays reloading
    * Force stop settings app during refresh (if isn't on foreground)
    * Reload adb notification during refresh
    * Cleanups
    
    Change-Id: Ib054c0503f1f07a9c19bf28dc11cc93b6a5923d0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -663,6 +663,10 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     // Dark theme style
     private boolean mUseBlackTheme;
+    private int mThemeMode;
+    private boolean mThemeAutomaticTimeIsNight;
+    private boolean shouldReloadOverlays = true;
+    private ActivityManager mActivityManager;
 
     @Override
     public void start() {
@@ -741,6 +745,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         mLockPatternUtils = new LockPatternUtils(mContext);
 
         mMediaManager.setUpWithPresenter(this, mEntryManager);
+
+        mActivityManager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
 
         // Connect in to the status bar manager service
         mCommandQueue = getComponent(CommandQueue.class);
@@ -1278,6 +1284,9 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     @Override
     public void onOverlayChanged() {
+        if (!shouldReloadOverlays){
+            return;
+        }
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onOverlayChanged();
         }
@@ -4132,6 +4141,27 @@ public class StatusBar extends SystemUI implements DemoMode,
         return true;
     }
 
+    private void forceStopSettingsIfNeeded(){
+        boolean shouldForceStop;
+        if (!mPowerManager.isInteractive() || isKeyguardShowing()){
+            shouldForceStop = true;
+        }else{
+            List<ActivityManager.RunningTaskInfo> taskInfo = mActivityManager.getRunningTasks(1);
+            ActivityManager.RunningTaskInfo foregroundApp = null;
+            if (taskInfo != null && !taskInfo.isEmpty()) {
+                foregroundApp = taskInfo.get(0);
+            }
+            shouldForceStop = foregroundApp == null ||
+                    !foregroundApp.baseActivity.getPackageName().equals("com.android.settings");
+        }
+        if (shouldForceStop){
+            try{
+                mActivityManager.forceStopPackage("com.android.settings");
+            }catch(Exception ignored){
+            }
+        }
+    }
+
     protected void updateTheme(boolean fromPowerSaveCallback, boolean themeNeedsRefresh) {
         final boolean inflated = mStackScroller != null && mStatusBarWindowManager != null;
         final UiModeManager umm = mContext.getSystemService(UiModeManager.class);
@@ -4141,8 +4171,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         if ((fromPowerSaveCallback || !darkThemeNeeded) && mNightModeInBatterySaver && mIsOnPowerSaveMode){
             darkThemeNeeded = true;
         }
+        if (mThemeMode == Settings.Secure.THEME_MODE_TIME){
+            darkThemeNeeded = mThemeAutomaticTimeIsNight;
+        }
         final boolean useDarkTheme = darkThemeNeeded;
         if (themeNeedsRefresh || isUsingDarkTheme() != useDarkTheme) {
+            forceStopSettingsIfNeeded();
+            shouldReloadOverlays = false;
             mUiOffloadThread.submit(() -> {
                 umm.setNightMode(useDarkTheme ? UiModeManager.MODE_NIGHT_YES : UiModeManager.MODE_NIGHT_NO);
                 try {
@@ -4170,6 +4205,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             mUiOffloadThread.submit(() -> {
                 swapWhiteBlackAccent();
             });
+            mHandler.postDelayed(() -> {
+                shouldReloadOverlays = true;
+                onOverlayChanged();
+            }, 1000);
         }
 
         // Lock wallpaper defines the color of the majority of the views, hence we'll use it
@@ -4889,6 +4928,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.THEME_DARK_STYLE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.THEME_AUTOMATIC_TIME_IS_NIGHT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.THEME_MODE),
+                    false, this, UserHandle.USER_ALL);
         }
          @Override
         public void onChange(boolean selfChange, Uri uri) {
@@ -4896,10 +4941,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (uri.equals(Settings.System.getUriFor(Settings.System.THEME_DARK_STYLE))) {
                 updateDarkThemeStyle();
                 updateTheme(false, true);
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.THEME_AUTOMATIC_TIME_IS_NIGHT)) ||
+                        uri.equals(Settings.Secure.getUriFor(Settings.Secure.THEME_MODE))) {
+                updateSettings();
+                updateTheme(false, false);
             }
         }
 
          public void update() {
+            updateSettings();
             setHeadsUpStoplist();
             setHeadsUpBlacklist();
             setQsPanelOptions();
@@ -4908,6 +4958,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             setStatusDoubleTapToSleep();
             updateDarkThemeStyle();
         }
+    }
+
+    private void updateSettings(){
+        mThemeMode = Settings.Secure.getInt(
+                mContext.getContentResolver(),
+                Settings.Secure.THEME_MODE, Settings.Secure.THEME_MODE_WALLPAPER);
+        mThemeAutomaticTimeIsNight = Settings.System.getInt(
+                mContext.getContentResolver(),
+                Settings.System.THEME_AUTOMATIC_TIME_IS_NIGHT, 0) != 0;
     }
 
     private void setHeadsUpStoplist() {
